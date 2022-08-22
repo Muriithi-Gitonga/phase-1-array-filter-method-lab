7ded7ff27e75f1da75fff3b37813d076995514ce
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-08-22 07:17:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(driverArray, string) {
+    return driverArray.filter((array) => { 
+        return array.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(driverArray, string) {
+    return driverArray.filter((array) => { 
+        return array.toUpperCase().substring(0, string.length) === string.toUpperCase() 
+    })
+}
+  
+function matchName(driverArray, string) {
+    return driverArray.filter((array) => { 
+        return array.name === string
+    })
+  }
